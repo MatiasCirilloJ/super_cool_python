f21a5d283811267e1a9b4bb56522a3242f46e239
mcirillo <75842869+MatiasCirilloJ@users.noreply.github.com>
2021-05-17 13:30:00 -0300

    Update hello_st2.py

--- actions/hello_st2.py
+++ actions/hello_st2.py
@@ -1,8 +1,9 @@
-import functions
+#import functions
+import sys
 
 from st2common.runners.base_action import Action
 
 class HelloStackStorm(Action):
     def run(self):
-        print("Hello Stackstorm, want to be friends?")
+        print(sys.path)
         return True
