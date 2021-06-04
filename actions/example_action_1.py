aa0748177858cb86344f2b04fd925b5dd5ebc418
mcirillo <75842869+MatiasCirilloJ@users.noreply.github.com>
2021-06-04 10:39:59 -0300

    Update example_action_1.py

--- actions/example_action_1.py
+++ actions/example_action_1.py
@@ -3,5 +3,5 @@ from st2common.runners.base_action import Action
 class HelloStackStorm(Action):
     def run(self, name):
         print(name)
-        resultado = {"resultado1": "res1", "resultado2": "res2"}
+        resultado = {"host_ip": "res1", "resultado2": "res2"}
         return (True, resultado)
