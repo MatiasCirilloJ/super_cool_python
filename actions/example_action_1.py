ec2606f5b2e8589a05406ce576cd606cf5bdc6cd
mcirillo <75842869+MatiasCirilloJ@users.noreply.github.com>
2021-06-04 10:13:33 -0300

    Update example_action_1.py

--- actions/example_action_1.py
+++ actions/example_action_1.py
@@ -1,6 +1,6 @@
 from st2common.runners.base_action import Action
 
 class HelloStackStorm(Action):
-    def run(self):
-        print("Hello Stackstorm, want to be friends?")
+    def run(self, name):
+        print(name)
         return True, "Matias"
