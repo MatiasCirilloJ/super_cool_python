e439ae5f0d8e16d7c0f6d35f29799525c2f08170
mcirillo <75842869+MatiasCirilloJ@users.noreply.github.com>
2021-06-08 10:50:32 -0300

    Update example_action_2.py

--- actions/example_action_2.py
+++ actions/example_action_2.py
@@ -2,5 +2,5 @@ from st2common.runners.base_action import Action
 
 class HelloStackStorm(Action):
     def run(self, name2):
-        print(name2 + " | Funca")
+        print(name2)
         return (True)
