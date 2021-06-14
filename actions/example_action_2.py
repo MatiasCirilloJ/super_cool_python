c0e65a44a437ab688435c832e7e8617d5bd6f8ef
MatiCirillo <matias.cirillo@la.logicalis.com>
2021-06-14 10:46:25 -0300

    bucle task2

--- actions/example_action_2.py
+++ actions/example_action_2.py
@@ -1,6 +1,8 @@
 from st2common.runners.base_action import Action
 
 class HelloStackStorm(Action):
-    def run(self, name2):
+    def run(self, name2, key = False):
         print(name2)
+        if key:
+            return (False)
         return (True)
