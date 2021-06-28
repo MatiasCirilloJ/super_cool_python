d57f0eed59814068ab5837e4a0ee435a80a5944f
MatiCirillo <matias.cirillo@la.logicalis.com>
2021-06-28 19:20:17 -0300

    a

--- actions/viewer.py
+++ actions/viewer.py
@@ -28,7 +28,7 @@ class EchoRemote(Action):
             vm = service_data[host]['VM']
             vm_status = vm_remed(vm, False)
             if vm_status:
-                return (True, service_data[host]['host'])
+                return (True, service_data[host]['host'].replace("'",""))
             else:
                 return (False, "deadman-host={} status CRITICAL".format(host))
 
