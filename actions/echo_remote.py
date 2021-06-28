284bc9cae8404da6ae2b4eaad38d5e5ec9273503
MatiCirillo <matias.cirillo@la.logicalis.com>
2021-06-28 17:26:19 -0300

    a

--- actions/echo_remote.py
+++ actions/echo_remote.py
@@ -6,7 +6,7 @@ from st2common.runners.base_action import Action
 class EchoRemote(Action):
     def run(self, hosts, username, private_key, cmd):
         print()
-        execution = subprocess.check_output("st2 run core.remote hosts='{}' username='{}' private_key='{}' cmd='{}'".format(hosts, username, private_key, cmd), shell=True)
+        execution = subprocess.check_output("st2 run core.remote hosts='{}' username='{}' private_key='{}' cmd='{}' -j".format(hosts, username, private_key, cmd), shell=True)
         print(json.loads(execution))
         #id_execution = json.loads(execution)[0]["id"]
         return (False)
